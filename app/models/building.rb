615c67b0b60c6f6ce3bf0ad2faef83adc39137e0
tosgit1989 <purannaa1989@gmail.com>
2016-01-29 16:46:49 +0900

    BuildingモデルとReviewモデルの関係について宣言

--- app/models/building.rb
+++ app/models/building.rb
@@ -1,2 +1,3 @@
 class Building < ActiveRecord::Base
+  has_many :reviews # 建物は自身に関するレビューを複数持っている。よって、Buildingモデルは複数のReviewモデルに所属
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,2 +1,3 @@
 class Review < ActiveRecord::Base
+  belongs_to :building # レビューはある特定の建物1つに関してのもの。よって、Reviewモデルは1つのBuildingモデルに所属
 end
