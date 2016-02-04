1260bc9e7fa6bcfef4985cc984f767f369ab0e65
tosgit1989 <purannaa1989@gmail.com>
2016-02-04 09:49:08 +0900

    configure_permitted_parametersメソッド内で、sign_upアクション実行時にnicknameをパラメーターとして許可する処理を書いた。 deviseコントローラーのアクションが動いた時のみconfigure_permitted_parametersメソッドが働く処理を書いた。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  def after_sign_out_path_for(resource)
-    '/users/sign_in' # サインアウト後のリダイレクト先URL
+  before_action :configure_permitted_parameters, if: :devise_controller? # deviseコントローラーのアクションが動いた時のみ、configure_permitted_parametersメソッドが働く
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up).push(:nickname) # sign_upアクション実行時にnicknameをパラメーターとして許可
   end
 end
