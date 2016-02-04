abc4e47c8681c7a77dd10113bcd875c7a77ecdba
tosgit1989 <purannaa1989@gmail.com>
2016-02-04 21:33:39 +0900

    UserモデルとReviewモデルの関係について宣言

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,6 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  has_many :reviews # ユーザーは自身が投稿したことのある建物の数に応じてレビューを持っている。よって、Userモデルは複数のReviewモデルに所属
   validates :nickname, presence: true # userのnicknameを入力必須に
 end
