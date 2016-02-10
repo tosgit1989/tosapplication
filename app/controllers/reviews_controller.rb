a3f5410c1519cec9ef09052e9c361d275773ef13
tosgit1989 <purannaa1989@gmail.com>
2016-02-10 22:16:32 +0900

    自分の投稿したレビューをマイページにて編集できるようにした。

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,19 +1,33 @@
 class ReviewsController < RankingController
   before_action :authenticate_user!, only: :new # ログインしていない時にレビューを投稿しようとすると、ログイン画面にリダイレクト
-  def new
+
+  def new # レビュー投稿画面
     @building = Building.find(params[:building_id])
     @review = Review.new
   end
 
-  def create
-    # reviews_controller.rbのcreateアクションでレビューデータを受け取り、データベースのReviewsテーブルに保存
-    Review.create(create_params)
-    # トップページにリダイレクトする
-    redirect_to controller: :buildings, action: :index
+  def create # レビュー投稿機能
+    Review.create(create_params) # reviews_controller.rbのcreateアクションでレビューデータを受け取り、データベースのReviewsテーブルに保存
+    redirect_to controller: :buildings, action: :index # トップページにリダイレクトする
+  end
+
+  def edit # レビュー編集機能
+    @review = Review.find(params[:id])
+    @building = @review.building
+  end
+
+  def update # レビュー更新機能
+    @review = Review.find(params[:id])
+    @building = @review.building
+    @review.update(update_params)
   end
 
   private
   def create_params
+    params.require(:review).permit(:rate, :review).merge(building_id: params[:building_id], user_id: current_user.id)
+  end
+
+  def update_params
     params.require(:review).permit(:rate, :review).merge(building_id: params[:building_id], user_id: current_user.id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,11 @@ Rails.application.routes.draw do
     # 以下の2行により投稿画面のルーティングを設定、投稿画面を表示できるように
     get 'buildings/:building_id/reviews/new' => 'reviews#new'
     post 'buildings/:building_id/reviews' => 'reviews#create'
-    
+
     devise_for :users
     resources :users, only: :show
     resources :buildings, only: :show do
-    resources :reviews, only: [:new, :create]
+    resources :reviews, only: [:new, :create, :edit, :update]
     collection do
       get 'search'
     end
