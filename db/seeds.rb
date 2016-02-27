20591ef51d657dc0e64466269df39547d7569459
tosgit1989 <purannaa1989@gmail.com>
2016-02-27 14:34:26 +0900

    add csv

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+require "csv"
+
+users_csv = CSV.readlines("db/users.csv")
+users_csv.shift
+users_csv.each do |row|
+  User.create(email: row[1], encrypted_password: row[2], nickname: row[13])
+end
