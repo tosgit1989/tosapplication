61d2c93cc7db7ee151a45314753614997618b786
tosgit1989 <purannaa1989@gmail.com>
2016-04-21 23:09:07 +0900

    review_averageをreview_rate_averageに書き換え("レートの平均"であるとわかるようにするため)

--- app/models/building.rb
+++ app/models/building.rb
@@ -1,7 +1,7 @@
 class Building < ActiveRecord::Base
   has_many :reviews # 建物は自身に関するレビューを複数持っている。よって、Buildingモデルは複数のReviewモデルに所属
   
-  def review_average
+  def review_rate_average
     self.reviews.average(:rate).round
   end
 
