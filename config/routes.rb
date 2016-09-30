c297974bd52ae5322173a69fbc356e2aaff52f73
tosgit1989 <purannaa1989@gmail.com>
2016-10-01 07:14:14 +0900

    search2.html.erbファイル削除、それに伴いroutes.rbで "get 'search2' " 削除、building_controller.rbで "def search2 〜 end" 削除

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
     # 以下の2行により投稿画面のルーティングを設定、投稿画面を表示できるように
     get 'buildings/:building_id/reviews/new' => 'reviews#new'
     post 'buildings/:building_id/reviews' => 'reviews#create'
+    get 'api/get_buildings' => 'reviews#new'
 
     devise_for :users
     resources :users, only: :show
@@ -9,7 +10,6 @@ Rails.application.routes.draw do
     resources :reviews, only: [:new, :create, :destroy, :edit, :update]
     collection do
       get 'search1'
-      get 'search2'
     end
   end
   root 'buildings#index'
