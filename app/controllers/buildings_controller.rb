ff47f7118f9fe63f8215d3caafd7684054444782
tosgit1989 <purannaa1989@gmail.com>
2016-10-22 08:04:29 +0900

    検索結果の件数が正確に表示されるようにした

--- app/controllers/buildings_controller.rb
+++ app/controllers/buildings_controller.rb
@@ -12,7 +12,9 @@ class BuildingsController < RankingController
 
   def search1
     # 検索フォームのキーワードをあいまい検索して、buildingsテーブルから建物情報を取得する
-    @buildings = Building.where('address LIKE(?)', "%#{params[:keypref]}%").where('building_name LIKE(?)', "%#{params[:keyword]}%").where('detail LIKE(?)', "%#{params[:keydeta]}%").page(params[:page]).per(20)
+    @buildings_a = Building.where('address LIKE(?)', "%#{params[:keypref]}%").where('building_name LIKE(?)', "%#{params[:keyword]}%").where('detail LIKE(?)', "%#{params[:keydeta]}%")
+    @buildings_b = @buildings_a.page(params[:page]).per(20)
+    
   end
 
 end
